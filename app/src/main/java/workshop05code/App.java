1246650a895adf033dfd70483500ab4cb20e3d38
shreyasreddy04 <shreyasreddy.raichandrashekarreddy@students.mq.edu.au>
2025-03-27 07:12:04 +1100

    Exercise 5.3 fixed solution for invalid input

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -74,15 +74,17 @@ public class App {
             String guess = scanner.nextLine();
 
             while (!guess.equals("q")) {
-                System.out.println("You've guessed '" + guess+"'.");
-
-                if (wordleDatabaseConnection.isValidWord(guess)) { 
-                    System.out.println("Success! It is in the the list.\n");
-                }else{
-                    System.out.println("Sorry. This word is NOT in the the list.\n");
+                if (!guess.matches("^[a-z]{4}$")) {
+                    System.out.println("Invalid input. Please enter a 4-letter word with only lowercase letters (a-z).");
+                } else {
+                    System.out.println("You've guessed '" + guess + "'.");
+                    if (wordleDatabaseConnection.isValidWord(guess)) { 
+                        System.out.println("Success! It is in the list.\n");
+                    } else {
+                        System.out.println("Sorry. This word is NOT in the list.\n");
+                    }
                 }
-
-                System.out.print("Enter a 4 letter word for a guess or q to quit: " );
+                System.out.print("Enter a 4 letter word for a guess or q to quit: ");
                 guess = scanner.nextLine();
             }
         } catch (NoSuchElementException | IllegalStateException e) {
